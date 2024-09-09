949567624da514f08ce1e811f48905775548825b
vijay <vijayasagarin@yahoo.com>
2024-09-09 14:47:10 -0500

    Revert "Changed the endpoint to demos"
    This reverts commit af5c4dfcd21dd5a30d0ddd6afdb55ac41e4d8eb1.

--- src/main/java/com/example/controller/DemoController.java
+++ src/main/java/com/example/controller/DemoController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DemoController {
 
-    @GetMapping("/demos")
+    @GetMapping("/demo")
     public String getDemoDetails() {
         return "This is a demo";
     }
