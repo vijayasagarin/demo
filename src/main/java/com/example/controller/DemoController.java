bc6bed7dc0e5ab98936db91ab0204d1ce4a1b3fc
vijay <vijayasagarin@yahoo.com>
2024-09-09 14:46:21 -0500

    Changed the endpoint to demos

--- src/main/java/com/example/controller/DemoController.java
+++ src/main/java/com/example/controller/DemoController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DemoController {
 
-    @GetMapping("/demos")
+    @GetMapping("/demossssss")
     public String getDemoDetails() {
         return "This is a demo";
     }
